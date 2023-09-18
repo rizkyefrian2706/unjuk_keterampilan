c68554886edcbf9084f6d943962593e50364ef2b
ATS.Rizky Efrian <ats.rizkye@xl.co.id>
2023-09-18 15:55:54 +0700

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/cek_status.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Image.asset(
+          'assets/xl.png',
+          width: 35,
+          height: 35,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(15.0),
+          children: <Widget>[
+            Stack(children: [
+              Image.asset(
+                'assets/landing1.png',
+                height: 400,
+                width: 400,
+                fit: BoxFit.fitWidth,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
+                child: Center(
+                    child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Card(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: SizedBox(
+                            width: 150,
+                            height: 200,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/sp.png',
+                                          width: 50,
+                                          height: 50,
+                                        ),
+                                        const Expanded(
+                                            child: Text(
+                                          'Pesan Kartu Perdana',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        )),
+                                      ],
+                                    ),
+                                    const Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 20, 0, 0)),
+                                    const Expanded(
+                                        child: Text(
+                                            'Pesan sekarang dan tunggu kartu perdana sampai dirumah Anda')),
+                                    const Divider(
+                                      thickness: 1,
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30.0),
+                                          ),
+                                          padding: const EdgeInsets.all(20),
+                                          backgroundColor: const Color.fromRGBO(
+                                              0, 45, 187, 1)),
+                                      onPressed: () {},
+                                      child: const Text(
+                                        'Pesan Disini',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Card(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: SizedBox(
+                            width: 150,
+                            height: 200,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/pesanan.png',
+                                          width: 50,
+                                          height: 50,
+                                        ),
+                                        const Expanded(
+                                            child: Text(
+                                          'Cek Status Pesanan',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        )),
+                                      ],
+                                    ),
+                                    const Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 20, 0, 0)),
+                                    const Expanded(
+                                        child: Text(
+                                            'Sudah melakukan pesanan? Cek status pesananmu')),
+                                    const Divider(
+                                      thickness: 1,
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30.0),
+                                          ),
+                                          padding: const EdgeInsets.all(20),
+                                          backgroundColor: const Color.fromRGBO(
+                                              0, 45, 187, 1)),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (_) =>
+                                                  const CekStatus()),
+                                        );
+                                      },
+                                      child: const Text(
+                                        'Cek Disini',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Card(
+                      child: SizedBox(
+                        width: 310,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                            "Cara order starter pack",
+                            textAlign: TextAlign.left,
+                          ),
+                          Icon(Icons.arrow_drop_down)
+                            ],
+                          )
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+            ]),
+          ]),
+      bottomNavigationBar: const Card(
+        color: Color.fromRGBO(0, 210, 160, 1),
+        child: SizedBox(
+          width: 1500,
+          height: 55,
+          child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    "Copyright @ 2023 XL Axiata",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "Privacy Policy | Terms & Conditions",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
